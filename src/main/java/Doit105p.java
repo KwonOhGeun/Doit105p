7c24bee89f43aeec1a1109fc51b87561f04cbb3d
KwonOhGeun <101384390+KwonOhGeun@users.noreply.github.com>
2022-04-05 21:11:20 +0900

    Initial commit

---
+++ src/main/java/Doit105p.java
@@ -0,0 +1,19 @@
+public class Doit105p {
+    public static void main(String[] args) {
+        String medal = "Gold";
+
+        switch (medal) {
+            case "Gold":
+                System.out.println("금메달입니다.");
+                break;
+            case "Silver":
+                System.out.println("은메달입니다.");
+            case "Bronze":
+                System.out.println("동메달입니다.");
+                break;
+            default:
+                System.out.println("메달이 없습니다.");
+                break;
+        }
+    }
+}
